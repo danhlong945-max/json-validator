f7596c2374fcd2607134bed79b4534add5458569
danhlong945-max <danhlong945-max@users.noreply.github.com>
2026-06-19 21:53:33 +0700

    Initial commit: JSON schema validator tool

---
+++ main.py
@@ -0,0 +1,9 @@
+"""JSON schema validator tool"""
+import sys
+
+def main():
+    print("json-validator - JSON schema validator tool")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
